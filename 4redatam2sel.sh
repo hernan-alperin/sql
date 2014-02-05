e14a41ff47d6ed8057001e41bf76e3c74e278aca
Hernán Alperin <hernan.alperin@gmail.com>
2014-02-05 15:13:24 -0200

    elimina el espacio inicial de cada línea
    psql -A

--- 4redatam2sel.sh
+++ 4redatam2sel.sh
@@ -6,6 +6,6 @@ psql -tc "select distinct $columnas from $tabla order by $columnas" sig_20 > lin
 sed "s/ \(.*\)/select '0104******0'||link from $tabla where $columnas='\1';___\1/" lineas |\
 #sed -e 's/á/a/g' -e 's/é/e/g' -e 's/í/i/g' -e 's/ó/o/g' -e 's/ú/u/g' -e 's/ü/u/g' -e 's/ñ/n/g' |\
 #sed -e 's/Á/A/g' -e 's/É/E/g' -e 's/Ì/I/g' -e 's/Ò/O/g' -e 's/Ù/U/g' -e 's/Û/U/g' -e 's/Ñ/N/g' |\
-sed 's/\(.*\)___\(.*\)/echo "*R+SP 2 \2" > "\2.sel"; psql -tc "\1" sig_20 >> "\2.sel"/'  > comandos
+sed 's/\(.*\)___\(.*\)/echo "*R+SP 2 \2" > "\2.sel"; psql -Atc "\1" sig_20 >> "\2.sel"/'  > comandos
 
 source comandos
